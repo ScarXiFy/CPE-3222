5375f48637361232d158db46c8b68c20db02bc97
John Enrico Lauron <johnenrico0828@gmail.com>
2026-03-09 22:33:58 +0800

    Refactor lauron_OddEven.php to improve table styling and layout

--- phpExercises/lauron_OddEven.php
+++ phpExercises/lauron_OddEven.php
@@ -1,17 +1,22 @@
 <?php
 
-echo "<table style='border-collapse: collapse; width: 50%; margin: 20px auto; font-family: Arial, sans-serif;'>";
+echo "<table border='1' style='border-collapse: collapse;'>";
+
 echo "<tr>";
-echo "<th style='border: 2px solid black; padding: 8px; text-align: center; background-color: green; color: white; font-size: 18px;'>Odd Numbers</th>";
-echo "<th style='border: 2px solid black; padding: 8px; text-align: center; background-color: green; color: white; font-size: 18px;'>Even Numbers</th>";
+echo "<th style='background-color: green; color: white;'>Odd Numbers</th>";
+echo "<th style='background-color: green; color: white;'>Even Numbers</th>";
 echo "</tr>";
 
 for ($i = 1; $i <= 100; $i += 2) {
+
     echo "<tr>";
-    echo "<td style='border: 2px solid black; padding: 8px; text-align: center; background-color: white;'>" . $i . "</td>";
-    if (($i + 1) <= 100) {
-        echo "<td style='border: 2px solid black; padding: 8px; text-align: center; background-color: white;'>" . ($i + 1) . "</td>";
+
+    echo "<td style='background-color: white;'>" . $i . "</td>";
+
+    if ($i + 1 <= 100) {
+        echo "<td style='background-color: white;'>" . ($i + 1) . "</td>";
     }
+
     echo "</tr>";
 }
 
