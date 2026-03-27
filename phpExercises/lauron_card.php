344ddc93b8d7cd03c39dc13eeea211e9c2c3a6d0
John Enrico Lauron <johnenrico0828@gmail.com>
2026-03-28 02:02:09 +0800

    Add credit card validation and display functionality in lauron_card.php

--- phpExercises/lauron_card.php
+++ phpExercises/lauron_card.php
@@ -1,5 +1,13 @@
 <?php
+    $creditCardNumber = "1234  5678  9101  1213";
+    echo "Credit Card Number: " . $creditCardNumber . "<br>";
 
+    // Removing the dashes and spaces from the credit card number
+    $creditCardNumber = str_replace(['-', ' '], '', $creditCardNumber);
 
-
+    // Checking if the credit card number is 16 digits long
+    if (preg_match('/^\d{16}$/', $creditCardNumber)) 
+        echo "TRANSACTION SUCCESSFUL";
+    else 
+        echo "TRANSACTION FAILED";
 ?>
